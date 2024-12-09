dcfdcd69aaaea7cb0aabf529af57334087e78865
Manu <sa.anamanuela@gmail.com>
2024-12-09 18:25:23 +0000

    new file:   Armazens.java 	new file:   GerirInventarios.java 	new file:   GestaoDeDados.java 	new file:   IoTTag.java 	new file:   Mercadorias.java 	new file:   Relatorios.java 	new file:   Transportes.java

---
+++ GerirInventarios.java
@@ -0,0 +1,25 @@
+import java.util.List;         // Para listas (ArrayList, LinkedList)
+import java.util.ArrayList;    // Para implementar List (pode ser usada em Goods, Warehouse, etc.)
+import java.util.HashMap;      // Para mapas de dados (caso precise de mapeamento chave-valor)
+import java.util.Map;          // Para o tipo Map
+import java.util.Date;         // Para manipulação de datas (caso precise de timestamps para movimentação)
+import java.io.*;              // Para entrada e saída de arquivos
+import java.text.SimpleDateFormat; // Para formatação de datas em relatórios (se necessário)
+
+
+//gestão geral dos armazéns e mercadorias
+public class GerirInventarios {
+    private List<Armazens> armazens;
+    
+    public void registarArmazem(Armazens armazens) { 
+
+     }
+
+    public Armazens encontrarArmazemPorNome(String nome) { 
+
+     }
+
+    public void moverMercadorias(Armazens origem, Armazens destino, Mercadorias mercadorias) { 
+
+    }
+}
---
+++ GestaoDeDados.java
@@ -0,0 +1,28 @@
+import java.util.List;         // Para listas (ArrayList, LinkedList)
+import java.util.ArrayList;    // Para implementar List (pode ser usada em Goods, Warehouse, etc.)
+import java.util.HashMap;      // Para mapas de dados (caso precise de mapeamento chave-valor)
+import java.util.Map;          // Para o tipo Map
+import java.util.Date;         // Para manipulação de datas (caso precise de timestamps para movimentação)
+import java.io.*;              // Para entrada e saída de arquivos
+import java.text.SimpleDateFormat; // Para formatação de datas em relatórios (se necessário)
+import com.google.gson.Gson;             // Para converter objetos Java em JSON e vice-versa
+import com.google.gson.reflect.TypeToken; // Para manipulação de tipos genéricos com Gson
+
+//Guardar e carregar dados de armazéns, mercadorias, transportes, etc, em FICHEIROS DE TEXTO
+public class GestaoDeDados {
+
+    //armazens
+    public void guardarDadosArmazens(Armazens armazens) { 
+
+     }
+    public Armazens carregarDadosArmazens(String ArmazensNome) { 
+
+    }
+
+    //marcadorias ....
+
+    //transportes ....
+
+    
+
+}
